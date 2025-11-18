c6b4ac1d9d77d52a0e2305ed82d5fda8537a7a60
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-18 17:25:07 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,31 @@
-"use client"
-import React, { useEffect, useState } from 'react'
+"use client";
+import React, { useEffect, useState } from "react";
 
 export default function App() {
-  const [products, setProducts] = useState([])
-
+  const [products, setProducts] = useState([]);
 
   const fetchProducts = async () => {
-    const res = await fetch("https://dummyjson.com/products")
+    const res = await fetch("https://dummyjson.com/products");
     const data = await res.json();
 
-
     if (data && data.products) {
-      setProducts(data.products)
+      setProducts(data.products);
     }
-  }
+  };
   console.log(products);
   useEffect(() => {
-    fetchProducts()
-  }, [])
+    fetchProducts();
+  }, []);
 
   return (
-    <div></div>
-  )
+    <div>
+      {products.length > 0 && (
+        <div className="text-2xl">
+          {products.map((prod) => {
+            return <span key={prod.id}>{prod.title}</span>;
+          })}
+        </div>
+      )}
+    </div>
+  );
 }
-
