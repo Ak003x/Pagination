b9f829ce5cb07f44440cb0fbdd9b008f4fde1f0c
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-18 20:14:50 +0530

    button

--- src/app/page.js
+++ src/app/page.js
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 
 export default function App() {
   const [products, setProducts] = useState([]);
+  const [page, setPage] = useState(1);
 
   const fetchProducts = async () => {
     const res = await fetch("https://api.escuelajs.co/api/v1/products");
@@ -21,19 +22,29 @@ export default function App() {
     <div>
       {products.length > 0 && (
         <div className="grid grid-cols-4 gap-5 p-5  bg-black ">
-          {products.map((prod) => {
+          {products.slice(page * 5 - 5, page * 5).map((prod) => {
             return (
               <span
                 key={prod.id}
                 className="p-5 text-xl text-center rounded-2xl bg-yellow-400 cursor-pointer "
               >
-                <img src={prod.images} alt={prod.id} className="object-cover"/>
+                <img src={prod.images} alt={prod.id} className="object-cover" />
                 <span>{prod.title}</span>
               </span>
             );
           })}
         </div>
       )}
+      <div>
+        <span>👈</span>
+        <span>
+          {products.length > 0 &&
+            [...Array(Math.floor(products.length / 5))].map((_, i) => {
+              return <span key={i}>{i + 1}</span>;
+            })}
+        </span>
+        <span>👉</span>
+      </div>
     </div>
   );
 }
