575ecbb76de360ef9513eed835169a38a0ecc476
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-18 15:50:41 +0530

    Added functions

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,23 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 
 export default function App() {
+  const [products, setProducts] = useState([])
+
+
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products")
     const data = await res.json();
-    console.log(data);
+
+
+    if (data && data.products) {
+      setProducts(data.products)
+    }
   }
-
-
+  console.log(products);
+  useEffect(() => {
+    fetchProducts()
+  }, [])
 
   return (
     <div></div>
